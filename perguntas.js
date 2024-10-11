eca008ee68a4142223605c264f6f21958a273a9d
nathpllz <161333624+nathpllz@users.noreply.github.com>
2024-10-11 09:15:23 -0300

    w

--- perguntas.js
+++ perguntas.js
@@ -1,11 +1,35 @@
 criaCartao(
-    'matemática',
-    'pergunta',
-    'resposta'
+    'BIOLOGIA',
+    'Para realizar seus experimentos, Mendel usou um organismo que apresenta curto tempo de geração, cultivo fácil e que gera grande número de descendentes. Que organismo foi esse?',
+    'Ervilha'
 )
 
 criaCartao(
     'matemática',
-    'pergunta',
-    'resposta'
+    'Caminhando ao fim da tarde, uma senhora contou 20 casas em uma rua à sua direita. No regresso, ela contou 20 casas à sua esquerda. Quantas casas ela viu no total?',
+    'rA senhora viu 20 casas no total, porque a sua direita na ida é a sua esquerda na volta, ou seja, nos dois itinerários, ela viu e contou as mesmas casas, e não casas diferentes.'
+)
+
+criaCartao(
+    'matemática',
+    'No meu jardim existe 3 pés de alface, 1 de pepino e 5 de cenoura. Quantos pés eu tenho no total?',
+    'Eu, como a maior parte das pessoas, tenho 2 pés.'
+)
+
+criaCartao(
+    'matemática',
+    'Qual a fórmula para calcular a área de um triângulo?',
+    'A = b*h/2'
+)
+
+criaCartao(
+    'matemática',
+    'Qual é o resultado de 5² + 7²?',
+    '74'
+)
+
+criaCartao(
+    'matemática',
+    'Qual é a fórmula para calcular o perímetro de um triângulo?',
+    'p= l+l+l'
 )
