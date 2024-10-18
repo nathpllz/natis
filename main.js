dbbe3d7246e92c5507fd689fb09d440ffe9a4d46
nathpllz <161333624+nathpllz@users.noreply.github.com>
2024-10-18 08:34:57 -0300

    z

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 function criaCartao(categoria,pergunta,resposta){
   let container = document.getElementById('container');
-  let CARTAO = document.createElement('article');
+  let cartao = document.createElement('article');
+  cartao.className = 'cartao';
+  cartao.innerHTML = `
+  
+  `
+
+
+}
