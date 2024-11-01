7cd40545ab3975da4b34146faba671f3e84e828f
nathpllz <161333624+nathpllz@users.noreply.github.com>
2024-11-01 11:17:25 -0300

    e

--- main.js
+++ main.js
@@ -14,14 +14,15 @@ function criaCartao(categoria,pergunta,resposta){
    
 </div>
   `
+  
 let respostaEstaVisivel=false;
 
 function viraCartao(){
   respostaEstaVisivel = !respostaEstaVisivel;
-  cartao.classList.toggle('active',respostaEstaVisivel)
+  cartao.classList.toggle('active', respostaEstaVisivel)
 }
-
 cartao.addEventListener('click', viraCartao);
+
  container.appendChild(cartao);
 
 }
