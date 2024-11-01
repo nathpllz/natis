b2a0fcfb8733503dffd12ac0a841feb4af83c8c6
nathpllz <161333624+nathpllz@users.noreply.github.com>
2024-11-01 11:22:38 -0300

    g

--- pagina2.js
+++ pagina2.js
@@ -18,7 +18,7 @@ function criaCartao(categoria,pergunta,resposta){
   }
 
   function mostarResposta(card){
-    const resposta=card.querySelector('.resposta-cartao');
+    const resposta= card.querySelector('.resposta-cartao');
 if(resposta.style.display==='none'){
     resposta.style.display='block'
 }
